0517bfbdd1a0d3715d27af88c873a116ba63c57d
OlehCharupa <65734895+OlehCharupa@users.noreply.github.com>
2021-02-16 10:52:39 +0200

    finish hw3

--- src/helpers/validate.js
+++ src/helpers/validate.js
@@ -5,7 +5,7 @@ class ValidateContacts {
     validateNewContact(req, res, next) {
         const validationRules = Joi.object({
             name: Joi.string().required(),
-            email: Joi.string().required(),
+            email: Joi.string().email().required(),
             phone: Joi.string().required(),
             subscription: Joi.string(),
             password: Joi.string().required()
