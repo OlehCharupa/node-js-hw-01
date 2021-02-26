c68784a883c849387b743600b444d433e80b35a0
OlehCharupa <65734895+OlehCharupa@users.noreply.github.com>
2021-02-26 23:39:35 +0200

    hw06

--- src/users/user.model.js
+++ src/users/user.model.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const { v4 } = require("uuid");
+const USER_STATUSES = require("./userStatuses");
 
 const userSchema = new Schema({
     email: {
@@ -18,6 +20,17 @@ const userSchema = new Schema({
         type: String,
         enum: ["free", "pro", "premium"],
         default: "free",
+    },
+    verificationToken: {
+        type: String,
+        required: false,
+        default: () => v4(),
+    },
+    status: {
+        type: String,
+        required: true,
+        enum: Object.values(USER_STATUSES),
+        default: USER_STATUSES.CREATED
     },
     token: String,
 });
